a724ded8572170d7e2c62859709aab9a419aa294
Phuong <phuongphi407@gmail.com>
2020-11-20 17:00:41 +0700

    Thêm result-search

--- src/main/webapp/app/shared/layout/visible/visible-component.tsx
+++ src/main/webapp/app/shared/layout/visible/visible-component.tsx
@@ -30,28 +30,45 @@ export const VisibleListSearch=(props)=>{
 
   const toggle = () => setDropdownOpen(prevState => !prevState);
   return(
-    <Form  className=" form-visible col d-flex">
-      <FormGroup className="col-auto">
-        <div className="d-none d-sm-none d-md-block d-lg-block d-xl-block">
-          <Dropdown isOpen={dropdownOpen} toggle={toggle} className="my-2 mr-1">
-            <DropdownToggle caret className="dropdown-toggle btn-secondary" >
-              Chọn danh mục
-            </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem header>Chọn danh mục</DropdownItem>
-              <DropdownItem>Đồ gỗ nội thất 1</DropdownItem>
-              <DropdownItem>Đồ gỗ nội thất 2</DropdownItem>
-              <DropdownItem>Đồ gỗ nội thất 3</DropdownItem>
-              <DropdownItem>Đồ gỗ nội thất 4</DropdownItem>
-              <DropdownItem>Đồ gỗ nội thất 5</DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
+    <Form  className=" form-visible col">
+      <div className="col d-flex">
+        <FormGroup className="col-auto">
+          <div className="d-none d-sm-none d-md-block d-lg-block d-xl-block">
+            <Dropdown isOpen={dropdownOpen} toggle={toggle} className="my-2 mr-1">
+              <DropdownToggle caret className="dropdown-toggle btn-secondary" >
+                Chọn danh mục
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header>Chọn danh mục</DropdownItem>
+                <DropdownItem>Đồ gỗ nội thất 1</DropdownItem>
+                <DropdownItem>Đồ gỗ nội thất 2</DropdownItem>
+                <DropdownItem>Đồ gỗ nội thất 3</DropdownItem>
+                <DropdownItem>Đồ gỗ nội thất 4</DropdownItem>
+                <DropdownItem>Đồ gỗ nội thất 5</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        </FormGroup>
+        <FormGroup className="col align-self-center">
+          <Input type="index" name="search" id="txtSearch" placeholder="Nhập từ khóa tìm kiếm..." className="d-none d-sm-none d-md-block d-lg-block d-xl-block "/>
+        </FormGroup>
+        <Button type="submit" className="btn-search d-none d-sm-none d-md-block d-lg-block d-xl-block col-auto ">Tìm kiếm</Button>
+      </div>
+      <div className="result-search col"> //todo cần sủa lại bằng reactstrap thay cho bootstrap
+        <div className="row mt-2">
+          <div className="col"><ResultSearch/></div>
+          <div className="col"><ResultSearch/></div>
         </div>
-      </FormGroup>
-      <FormGroup className="col align-self-center">
-        <Input type="index" name="search" id="txtSearch" placeholder="Nhập từ khóa tìm kiếm..." className="d-none d-sm-none d-md-block d-lg-block d-xl-block "/>
-      </FormGroup>
-      <Button type="submit" className="btn-search d-none d-sm-none d-md-block d-lg-block d-xl-block col-auto ">Tìm kiếm</Button>
+        <div className="row mt-2">
+          <div className="col"><ResultSearch/></div>
+          <div className="col"><ResultSearch/></div>
+        </div>
+        <div className="row mt-2">
+          <div className="col"><ResultSearch/></div>
+          <div className="col"><ResultSearch/></div>
+        </div>
+        <div className="float-right m-2">Xem tất cả</div>
+      </div>
     </Form>
   )
 }
@@ -67,3 +84,28 @@ export const WarpCart=(props)=>{
   )
 
 }
+
+import { Media } from 'reactstrap';
+
+export const ResultSearch = () => {
+  return (
+    // <Media>
+    //   <Media left href="#">
+    //     <Media object data-src="./../../../../content/images/do_go_san_pham_1.png/64x64" alt="Generic placeholder image" />
+    //   </Media>
+    //   <Media body>
+    //     <Media heading>
+    //       THÙNG GỖ SỒI MỘC
+    //     </Media>
+    //       2000000đ
+    //   </Media>
+    // </Media>
+    <div className="media border p-3">
+        <img src="./../../../../content/images/do_go_san_pham_1.png" alt="John Doe" className="mr-3 mt-3" width="80px"/>
+        <div className="media-body">
+          <h6>THÙNG GỖ SỒI MỘC</h6>
+          <p>2000000đ</p>
+        </div>
+    </div>
+  );
+};
